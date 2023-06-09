1d975bb50cf27296fab5a1abe4fe792b597b06f1
Miguel Manzano Álvarez <73229701+miguelma1@users.noreply.github.com>
2023-06-10 00:23:44 +0200

    feat: bootstrapping

---
+++ codigoUtil/bootstrapping.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import bootstrap
+
+
+titanicData = pd.read_csv('./datos/titanic.csv', skiprows = 1, header = None,
+                              names=['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked', 'Initial', 'Age_band', 'Family_Size', 'Alone', 'Fare_cat', 'Deck', 'Title', 'Is_Married', 'Survived'])
+
+
+def bootstrapData (fileName, data, amountNewFiles):
+    for i in range (amountNewFiles):
+                    #frac = 1 bootstrappea todo el dataSet, si quisiese el 50% haria frac = 0.5
+                    #replace = true puede repetir filas
+
+        bootstrap_sample = data.sample(frac = 1, replace = True)
+        route = f'./datos/conjutosEntrenamiento/{fileName}_trainSet_{i+1}.csv'
+        bootstrap_sample.to_csv(route, index = False)
+
+
+
+bootstrapData('titanic', titanicData, 3)
